0b85ac7d2511a9152321db26a4c24be378ae1b78
Anirudh Panchangam Ranganath <anirudh.panchangamranganath@study.thws.de>
2024-03-08 16:50:53 +0100

    :memo: Update website.ts

--- src/types/website.ts
+++ src/types/website.ts
@@ -1,8 +1,16 @@
 import { ReactElement } from "react";
 
 export type Website = {
-    name : string;
-    url : string;
-    colorTheme : string;
-    logo? : ReactElement
-}
+  name: string;
+  url: string;
+  SearchURL?: string;
+  colorTheme: string;
+  logo?: ReactElement;
+};
+
+export type Theme = {
+  name?: string;
+  websiteNameColor?: string;
+  bgColor?: string;
+  textColor?: string;
+};
